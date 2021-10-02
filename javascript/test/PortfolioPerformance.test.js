50871c90552e2d04affe3171e5b8af1ad00d66bf
Robert Bain <robert.bain@nucleusfinancial.com>
2021-10-02 18:16:01 +0100

    Fixing unit test

--- javascript/test/PortfolioPerformance.test.js
+++ javascript/test/PortfolioPerformance.test.js
@@ -2,12 +2,12 @@ import { getDailyPortfolioValues } from "../src/PortfolioPerformance";
 
 test("output is correct", () => {
     expect(getDailyPortfolioValues()).toEqual([
-        { effectiveDate: new Date(2021, 8, 1, 1), value: 177.32265 },
-        { effectiveDate: new Date(2021, 8, 2, 1), value: 178.2938 },
-        { effectiveDate: new Date(2021, 8, 3, 1), value: 185.55555 },
-        { effectiveDate: new Date(2021, 8, 4, 1), value: 816.44442 },
-        { effectiveDate: new Date(2021, 8, 5, 1), value: 445.33332 },
-        { effectiveDate: new Date(2021, 8, 6, 1), value: 456.49764 },
-        { effectiveDate: new Date(2021, 8, 7, 1), value: 3811.31632 },
+        { effectiveDate: "2021-09-01", value: 177.32265 },
+        { effectiveDate: "2021-09-02", value: 178.2938 },
+        { effectiveDate: "2021-09-03", value: 185.55555 },
+        { effectiveDate: "2021-09-04", value: 816.44442 },
+        { effectiveDate: "2021-09-05", value: 445.33332 },
+        { effectiveDate: "2021-09-06", value: 456.49764 },
+        { effectiveDate: "2021-09-07", value: 3811.31632 },
     ]);
 });
