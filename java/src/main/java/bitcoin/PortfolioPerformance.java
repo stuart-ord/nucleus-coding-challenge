60fc28c0e9090637ba9759a2cf00dbc08b40df83
Stuart Ord <stuart.ord.7@gmail.com>
2021-10-06 15:06:23 +0100

    Variable declarations

--- java/src/main/java/bitcoin/PortfolioPerformance.java
+++ java/src/main/java/bitcoin/PortfolioPerformance.java
@@ -1,12 +1,15 @@
 package bitcoin;
 
+// Imported packages
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PortfolioPerformance {
-
+	// Static list of prices
     private static final List<Price> PRICES = List.of(
             new Price(LocalDateTime.of(2021, Month.SEPTEMBER, 1, 5, 0, 0), new BigDecimal("35464.53")),
             new Price(LocalDateTime.of(2021, Month.SEPTEMBER, 2, 5, 0, 0), new BigDecimal("35658.76")),
@@ -15,6 +18,7 @@ public class PortfolioPerformance {
             new Price(LocalDateTime.of(2021, Month.SEPTEMBER, 6, 5, 0, 0), new BigDecimal("38041.47")),
             new Price(LocalDateTime.of(2021, Month.SEPTEMBER, 7, 5, 0, 0), new BigDecimal("34029.61")));
 
+    // Static list of transactions
     private static final List<Transaction> TRANSACTIONS = List.of(
             new Transaction(LocalDateTime.of(2021, Month.SEPTEMBER, 1, 9, 0, 0), new BigDecimal("0.012")),
             new Transaction(LocalDateTime.of(2021, Month.SEPTEMBER, 1, 15, 0, 0), new BigDecimal("-0.007")),
@@ -22,17 +26,26 @@ public class PortfolioPerformance {
             new Transaction(LocalDateTime.of(2021, Month.SEPTEMBER, 5, 9, 0, 0), new BigDecimal("-0.01")),
             new Transaction(LocalDateTime.of(2021, Month.SEPTEMBER, 7, 9, 0, 0), new BigDecimal("0.1")));
 
-    // Return a list of daily portfolio values with one record for each day 
+    // Return a list of daily portfolio values with one record for each day
     // From the 01-09-2021 to the 07-09-2021
     public static List<DailyPortfolioValue> getDailyPortfolioValues() {
         // Declaring variables
-
-
+    	LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 1);
+    	LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 7);
+    	BigDecimal portfolioValue = new BigDecimal("0");  
+    	BigDecimal currentPrice = new BigDecimal("0");
+    	List<DailyPortfolioValue> dailyPortfolioValues = new ArrayList<DailyPortfolioValue>();
+    	
         // For loop for the date range
-
-
-        // Output list of daily portfolio values
-        
-
+    	// endDate plus 1 to iterate endDate inclusive
+    	for (LocalDate d = startDate; d.isBefore(endDate.plusDays(1)); d = d.plusDays(1)) {
+    		
+    	}
+    	
+    	// Console log for testing desired output
+    	System.out.print(dailyPortfolioValues);
+    	
+        // Output list of daily portfolio values    	
+    	return dailyPortfolioValues;
     }
 }
